58614b56e179dab2b45dde435910b2b02eb155f7
UrbanPistek <urban.pistek@gmail.com>
2023-04-25 23:13:46 -0600

    fixed bug with running ros2 node individually

--- docker/ros2_container/ros2_ws/src/ros2_utils/ros2_utils/list_topics_node.py
+++ docker/ros2_container/ros2_ws/src/ros2_utils/ros2_utils/list_topics_node.py
@@ -4,16 +4,16 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 class minimalNode(Node):
-
-    def __init__(self):
-        super().__init__('list_topics_node')
-        self.create_timer(5.0, self.list_topics)
-
+    
     def list_topics(self):
         self.get_logger().info(' Sourcing topics...')
         topic_list = self.get_topic_names_and_types()
         for info in topic_list:
             self.get_logger().info(f'topic: {info[0]}')
+
+    def __init__(self):
+        super().__init__('list_topics_node')
+        self.create_timer(5.0, self.list_topics)
 
 def main(args=None):
     rclpy.init(args=args)
